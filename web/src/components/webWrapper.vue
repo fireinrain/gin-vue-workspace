46ad400efcfdec7a4390120da4be4feac8cb2f76
lzyme.dev@gmail.com <lzyme.dev@gmail.com>
2024-07-03 03:50:48 +0800

    添加asynq iframe页面

---
+++ web/src/components/webWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="web-wrapper">
+    <iframe :src="url" class="iframe"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'webWrapper',
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.web-wrapper {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.iframe {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+</style>
---
+++ web/src/view/asynqStatus/asynq.vue
@@ -0,0 +1,19 @@
+<script>
+import webWrapper from "@/components/webWrapper.vue";
+
+export default {
+  name: "asynq.vue",
+  components: {
+    webWrapper
+  }
+}
+
+</script>
+
+<template>
+  <webWrapper url="https://asynq.131433.xyz"></webWrapper>
+</template>
+
+<style scoped lang="scss">
+
+</style>
